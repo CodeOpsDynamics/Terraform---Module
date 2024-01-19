479c1030d6e193baa575daa46cd1fac9210a420f
himanshu rai <himanshurai@himanshus-MacBook-Air.local>
2024-01-20 02:10:09 +0530

    Your commit message here

---
+++ VM/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_linux_virtual_machine" "vmconnect" {
+  for_each            = var.vm
+  name                = each.value.name
+  resource_group_name = data.azurerm_resource_group.rgconnect.name
+  location            = data.azurerm_resource_group.rgconnect.location
+  size                = "Standard_F2"
+  disable_password_authentication = "false"
+  #admin_username      = data.azurerm_key_vault_secret.username.value
+  #admin_password      = data.azurerm_key_vault_secret.password.value
+  admin_username      = "Adminuser1"
+  admin_password      = "S1!ummer@UPSC0909"
+  network_interface_ids = [
+    each.value.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
